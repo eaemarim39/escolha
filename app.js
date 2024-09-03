6d6c9d65f8fec1a09275dac6fd3b4bd818060c79
eaemarim <caiquemarim39@gmail.com>
2024-09-02 22:32:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ function revealOption(option) {
 
     switch(option) {
         case 1:
-            message = 'Vão me dar o salário do dia 20, PARABÉNS!';
+            message = 'Vai sair comigo sexta, PARABÉNS!';
             break;
         case 2:
-            message = 'Vão me enviar o salário do dia 5 PARABÉNS!';
+            message = 'Vai sair comigo sábado PARABÉNS!';
             break;
         case 3:
-            message = 'Vão me dar o salário inteiro PARABÉNS!';
+            message = 'Vai sair comigo domingo PARÁBENS!';
             break;
         default:
             message = 'Opção inválida!';
